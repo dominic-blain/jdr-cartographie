db3c8658359c98a5fe3da7f7f21970ac9e64a37f
Dominic Blain <dominic.blain@gmail.com>
2021-06-23 17:53:26 -0400

    Add placeholder footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,3 +4,17 @@
 
 <h1>JDR - Cartographie</h1>
 <slot></slot>
+<p>Pour contribuer (à venir)</p>
+
+<style>
+    h1 {
+        font-size: 32px;
+        line-height: 1.1;
+        margin: 0;
+        padding: 40px;
+    }
+    p {
+        font-size: 14px;
+        padding: 0 20px 10px;
+    }
+</style>
