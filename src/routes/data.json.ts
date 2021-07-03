6299fa732a87906ca30f6028a45a966dcb9bd7f5
Dominic Blain <dominic.blain@gmail.com>
2021-07-03 01:06:21 -0400

    Fix

--- src/routes/data.json.ts
+++ src/routes/data.json.ts
@@ -3,7 +3,7 @@ import { google } from 'googleapis'
 const APIKey = import.meta.env.VITE_GOOGLE_API_KEY as string
 
 export async function get() {
-  const sheets = google.sheets({ version: 'v4', auth: 'AIzaSyDx5u6LnQM6Cu4BJ5_VmpvazHjs6EHaajk' })
+  const sheets = google.sheets({ version: 'v4', auth: APIKey })
   return new Promise((resolve, reject) => {
     sheets.spreadsheets.values.get({
       spreadsheetId: '1nXRMnbQf3KPqE7B_dNjUA-sbJpMN8DYigf6ZMCtNnxI',
